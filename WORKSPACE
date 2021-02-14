61cea63f287f5552e132842d28257145d1f8886a
Pras Velagapudi <psigen@gmail.com>
2021-02-13 21:39:25 -0500

    Added a gRPC call and caused error.
    Creating a simple gRPC service and linking it seems to cause a
    conflict during the generation of the protofiles that I don't
    quite understand.
    
    ```
    Use --sandbox_debug to see verbose messages from the sandbox
    link: package conflict error: github.com/golang/protobuf/ptypes/any: multiple copies of package passed to linker:
    	@com_github_golang_protobuf//ptypes/any:any
    	@io_bazel_rules_go//proto/wkt:any_go_proto
    ```

--- WORKSPACE
+++ WORKSPACE
@@ -1,4 +1,4 @@
-workspace(name = "com_github_psigen_rules_go_2805")
+workspace(name = "com_github_psigen_bazel_go_issue")
 
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"github.com/gogo/status"
+	pb "github.com/psigen/bazel-go-issue/proto"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 )
 
 func main() {
+	// Use gogo Status to test linking.
 	foo := status.Error(codes.NotFound, "no such user")
 	_ = foo
-	return
+
+	// Create a client connection to the gRPC server.
+	conn, _ := grpc.Dial("localhost:10000")
+	client := pb.NewGreeterClient(conn)
+	_ = client
 }
